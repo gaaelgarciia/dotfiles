f9e75d8956ec0fb7d0227b008f07c15e95602e27
gaaelgarciia <gael.garciaa@udc.es>
2024-03-30 00:15:34 +0100

    Updated 30/03/24

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+    alias clase-Q4='cd $HOME/Desktop/Q4'
+    alias clase-Q3='cd $HOME/Desktop/Q3'
     set -g fish_greeting
     starship init fish | source
     # Commands to run in interactive sessions can go here
