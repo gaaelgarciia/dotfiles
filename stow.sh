71f633b08a25fa0c7e696197e39108ef741a49c7
gaaelgarciia <gael.garciaa@udc.es>
2026-03-11 17:32:51 +0100

    refactor

--- stow.sh
+++ stow.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
-echo "Select the desired config:"
-echo "[1] macos"
-echo "[2] sway"
-echo "[3] i3"
-echo "[4] general"
+while true; do
+    echo "Select the desired config:"
+    echo "[1] macos"
+    echo "[2] sway"
+    echo "[3] i3"
+    echo "[4] general"
+    read option
 
-read option
+    case "$option" in
+        1) dir="macos"; break ;;
+        2) dir="i3wm-x220"; break ;;
+        3) dir="sway-desktop"; break ;;
+        4) dir="general"; break ;;
+        *) echo "Invalid option, try again." ;;
+    esac
+done
 
-case "$option" in
-    1)
-        cd macos || exit
-        ;;
-    2)
-        cd i3wm-x220 || exit
-        ;;
-    3)
-        cd sway-desktop || exit
-        ;;
-    4)
-        cd general || exit
-        ;;
-    *)
-        echo "Invalid option"
-        exit 1
-        ;;
-esac
-
-stow .
+cd "$dir" || exit
+# -t ~/ to set the target to the home directory in case this was colned +1 depth than the home dir
+stow -t ~/ .
+# If there are existing code, --adopt is a good option
