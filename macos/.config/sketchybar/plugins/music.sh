68c45265c7b8b06a8915645625c048514c736416
gaaelgarciia <gael.garciaa@udc.es>
2026-05-29 12:37:30 +0200

    sketchybar: improvements in the music plugin

--- macos/.config/sketchybar/plugins/music.sh
+++ macos/.config/sketchybar/plugins/music.sh
@@ -3,12 +3,13 @@
 STATE_RMPC="$(rmpc status | jq -r .state)"
 SONG_RMPC="$(rmpc song | jq -r '"\(.metadata.artist) - \(.metadata.title)"')"
 
+IS_PLAYING="$(nowplaying-cli get-raw | jq -r ".kMRMediaRemoteNowPlayingInfoPlaybackRate")"
 SONG_GENERAL="$(nowplaying-cli get title)"
 ARTIST_GENERAL="$(nowplaying-cli get artist)"
 
-if [ "$STATE_RMPC" = "Play" ]; then
+if [ "$STATE_RMPC" == "Play" ]; then
     LABEL="$SONG_RMPC"
-elif [ "$SONG_GENERAL" != "null" ] && [ -n "$SONG_GENERAL" ]; then
+elif [ "$SONG_GENERAL" != "null" ] && [ "$IS_PLAYING" == "1" ] && [ -n "$SONG_GENERAL" ]; then
     LABEL="$SONG_GENERAL - $ARTIST_GENERAL"
 else
     LABEL="no music"
